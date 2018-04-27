466a8f770bb13db652ea03ea0a1203f63b5178ca
Stephanie Spears <Stephanie.Spears1@gmail.com>
2018-04-27 04:22:45 -0700

    init

---
+++ js/widget.js
@@ -0,0 +1,22 @@
+const xhr = new XMLHttpRequest();
+
+// xhr.open('GET', '../data/employees.json');
+
+xhr.onreadystatechange = function () {
+  if(xhr.readyState === 4 && xhr.status === 200) {
+    let employees = JSON.parse(xhr.responseText);
+    let statusHTML = '<ul class="bulleted">';
+    for (let i=0; i<employees.length; i++) {
+      if (employees[i].inoffice === true) {
+        statusHTML += '<li class="in">';
+      } else {
+        statusHTML += '<li class="out">';
+      }
+      statusHTML += employees[i].name;
+      statusHTML += '</li>';
+    }
+    statusHTML += '</ul>';
+    document.getElementById('employeeList').innerHTML = statusHTML;
+  }
+};
+xhr.send();
